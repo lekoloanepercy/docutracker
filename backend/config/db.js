22ee7d1ec1dda80505852e390039668a118889b8
Lekoloane Nape Percy <lekoloanenape5@gmail.com>
2026-03-15 03:06:23 +0200

    edit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,41 @@
-// db.js
+
+/*AVIEN CONNECTION*/
+const mysql = require("mysql2");
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
+
+const db = mysql.createPool({
+  host: process.env.AIVEN_DB_HOST,
+  user: process.env.AIVEN_DB_USER,
+  password: process.env.AIVEN_DB_PASS,
+  database: process.env.AIVEN_DB_NAME,
+  port: process.env.AIVEN_DB_PORT,
+
+  // Aiven requires SSL
+  ssl: {
+    ca: fs.readFileSync(path.resolve(__dirname, "ca.pem")),
+  },
+
+  // Pooling (keep small on free tier)
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0,
+});
+
+// Test connection
+db.getConnection((err, conn) => {
+  if (err) {
+    console.error("MySQL connection failed:", err.message);
+  } else {
+    console.log("MySQL Pool Connected to Aiven");
+    conn.release();
+  }
+});
+
+module.exports = db;
+
+/*nromal connection db.js
 const mysql = require('mysql2');
 require('dotenv').config();
 
@@ -17,5 +54,5 @@ connection.connect(err => {
   console.log('Connected to MySQL');
 });
 
-module.exports = connection;
+module.exports = connection;*/
 
